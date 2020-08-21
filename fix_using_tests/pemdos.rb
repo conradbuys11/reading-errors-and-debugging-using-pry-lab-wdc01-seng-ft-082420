b5c2145857f0027dd1e2d5585ead024d852f0328
Conrad Buys <conradbuys11@gmail.com>
2020-08-21 15:05:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,11 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    addbeforestring = ""
+    10.times do
+      addbeforestring += "s"
+    end
+    addbeforestring + string
   else
     string
   end
